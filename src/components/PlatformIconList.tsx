3401cd3f1bbae36651c2969f00ddfed8a08a7a22
Anomoly <greg.jenkins001@gmail.com>
2026-05-27 12:58:39 -0700

    displaying platform icons

---
+++ src/components/PlatformIconList.tsx
@@ -0,0 +1,43 @@
+import {
+  FaWindows,
+  FaPlaystation,
+  FaXbox,
+  FaApple,
+  FaLinux,
+  FaAndroid,
+} from "react-icons/fa";
+import { MdPhoneIphone } from "react-icons/md";
+import { BsNintendoSwitch, BsGlobe } from "react-icons/bs";
+import type { Platform } from "../hooks/useGames";
+import { HStack, Icon } from "@chakra-ui/react";
+import type { IconType } from "react-icons";
+
+interface PlatformIconListProps {
+  platforms: Platform[];
+}
+
+const PlatformIconList = ({ platforms }: PlatformIconListProps) => {
+  const iconMap: { [key: string]: IconType } = {
+    //name: PlayStation
+    //slug: playstation
+    pc: FaWindows,
+    playstation: FaPlaystation,
+    xbox: FaXbox,
+    nintendo: BsNintendoSwitch,
+    mac: FaApple,
+    linux: FaLinux,
+    ios: MdPhoneIphone,
+    web: BsGlobe,
+    android: FaAndroid,
+  };
+
+  return (
+    <HStack marginY={1}>
+      {platforms.map((platform) => (
+        <Icon as={iconMap[platform.slug]} color="gray.500" />
+      ))}
+    </HStack>
+  );
+};
+
+export default PlatformIconList;
